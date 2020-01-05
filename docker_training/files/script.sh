dd8cbd30f223c1112a8dcb3e6a79b05f05348035
gokultonpe <gokultonpe@gmail.com>
2020-01-05 01:18:55 -0800

    Add files via upload

---
+++ docker_training/files/script.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+#echo hello world, from a script file!
+#xpdf /resume_gntonpe_final.pdf
+FILE=/resume.txt
+clear
+echo "--------------------"
+echo "GOKUL TONPE's RESUME"
+echo "--------------------"
+cat $FILE
+
+
+
